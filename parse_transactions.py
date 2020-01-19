9f605d5e947259b8d1bb7e0a17321310880b68f9
Sam Keller <samskeller@gmail.com>
2020-01-18 19:51:54 -0800

    Add ability to save a merchant to categories.json file for later

--- parse_transactions.py
+++ parse_transactions.py
@@ -12,17 +12,38 @@ def load_categories():
         logging.exception("Cannot read the categories file")
         sys.exit()
 
-def determine_category(row, possibleCategories):
-    category_options_string = "\n".join([f"{num}: {category}" for num, category in enumerate(possibleCategories, 1)])
-    user_input_options = [str(i) for i in range(1, len(possibleCategories) + 1)]
-
-    prompt = f"\nPick a category for {row['Description']}:\n{category_options_string}\n\n"
+def parse_user_selected_category(prompt):
+    add_category = False
     user_selected_category = input(prompt)
+    if user_selected_category[-1] == '+':
+        add_category = True
+        user_selected_category = user_selected_category[:-1]
+
+    return (user_selected_category, add_category)
+
+def handle_unknown_category(row, possible_categories, category_mapper):
+    category_options_string = "\n".join([f"{num}: {category}" for num, category in enumerate(possible_categories, 1)])
+    user_input_options = [str(i) for i in range(1, len(possible_categories) + 1)]
+
+    prompt = f"\nPick a category for {row['Description']} (add '+' suffix to save):\n{category_options_string}\n\n"
+    user_selected_category, add_category = parse_user_selected_category(prompt)
     while user_selected_category not in user_input_options:
         print("Sorry, that's not an option. Try again!")
-        user_selected_category = input(prompt)
+        user_selected_category, add_category = parse_user_selected_category(prompt)
 
-    return possibleCategories[int(user_selected_category) - 1]
+    category = possible_categories[int(user_selected_category) - 1]
+    if add_category:
+        if row["Category"] not in category_mapper:
+            category_mapper[row["Category"]] = {}
+        category_mapper[row["Category"]][row["Description"]] = category
+
+    return category
+
+def add_category(internal_category, cap_one_category, description, category_mapper):
+    if cap_one_category not in category_mapper:
+        category_mapper[cap_one_category] = {}
+
+    category_mapper[cap_one_category][description] = internal_category
 
 def bucket_transactions(csv_reader):
     categories_blob = load_categories()
@@ -43,10 +64,18 @@ def bucket_transactions(csv_reader):
             else:
                 category = capital_one_category[row["Description"]]
         except KeyError:
-            category = determine_category(row, categories_blob["internalCategories"])
-            category = "Other"
+            category = handle_unknown_category(row, categories_blob["internalCategories"], category_lookup)
 
         totals[category] += float(row["Debit"])
+
+    # Write category_mapper back to categories.json
+    categories_blob["categoryMapper"] = category_lookup
+    try:
+        with open('categories.json', 'w') as output_categories:
+            output_categories.write(json.dumps(categories_blob, indent=2, sort_keys=True))
+    except IOError:
+        logging.exception("Cannot write to the categories file")
+        sys.exit()
 
     return totals
 
