bd87050a963cb9d573fc40619d916f6447213e0d
Sam Keller <samskeller@gmail.com>
2020-01-19 18:55:37 -0800

    Making plots to compare a month to the averages

--- parse_transactions.py
+++ parse_transactions.py
@@ -4,6 +4,10 @@ import argparse
 import csv
 import datetime
 import json
+import matplotlib
+matplotlib.use('TkAgg') # necessary for running via virtualenv on mac os x
+import matplotlib.pyplot as plt
+import numpy as np
 
 def read_categories():
     try:
@@ -79,6 +83,50 @@ def bucket_transactions(csv_reader):
 
     return (totals, transactions)
 
+def output_month_plot(month, averages, month_string):
+    categories = averages.keys()
+    monthly_amounts = [round(month[category]) for category in categories]
+    average_amounts = [round(averages[category]) for category in categories]
+    x = np.arange(len(categories))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, monthly_amounts, width, label=month_string)
+    rects2 = ax.bar(x + width/2, average_amounts, width, label='Average')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Amount ($)')
+    ax.set_xlabel('Category')
+    ax.set_title(f"Spending in {month_string} compared to average")
+    ax.set_xticks(x)
+    ax.set_xticklabels(categories)
+    ax.legend()
+
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom', fontsize="x-small")
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    # Make room for x-axis labels
+    plt.gcf().subplots_adjust(bottom=0.4)
+
+    # Rotate category names so they don't overlap
+    plt.xticks(rotation=90)
+
+    # Make sure y-axis is high enough to accommodate tall bar labels
+    tallest_bar = max(monthly_amounts + average_amounts)
+    plt.ylim(0, tallest_bar + (tallest_bar / 10))
+
+    plt.savefig(f"{month_string}.png")
+
 def get_date_from_filename(filename):
     date_string = filename.split('/')[-1][:len('2020-01-01')]
     return datetime.datetime.strptime(date_string, '%Y-%m-%d')
@@ -129,7 +177,6 @@ def main():
         totals = json.loads(totals_file.read())
         totals["months"][month_string] = monthly_totals
         overall_totals = {}
-        print(totals)
         for month_string, month in totals["months"].items():
             for category, amount in month.items():
                 overall_totals[category] = overall_totals.get(category, 0) + amount
@@ -139,6 +186,7 @@ def main():
         totals_file.write(json.dumps(totals, indent=2, sort_keys=True))
         totals_file.truncate()
 
+    output_month_plot(averages, monthly_totals, month_string)
 
 if __name__ == '__main__':
     main()
